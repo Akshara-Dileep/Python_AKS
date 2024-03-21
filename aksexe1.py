f675c26fceb642808b8849448e6681f416840711
Akshara-Dileep <164381154+Akshara-Dileep@users.noreply.github.com>
2024-03-21 17:07:04 +0000

    Add files via upload

---
+++ aksexe1.py
@@ -0,0 +1,11 @@
+r=int(input("Enter the radius of thr circle"))
+area=2*3.14*r
+print("area:",area)
+circumference=2*r
+print("circumference",circumference)
+
+'''output
+Enter the radius of thr circle 3
+area: 18.84
+circumference 6
+'''
